b752bd2fde315c14d3ac6c40a37d0871f1f1183c
Raff-dev <56380303+Raff-dev@users.noreply.github.com>
2020-12-19 21:18:36 +0100

    add get entries view

--- CovidAnalysis/FunctionalPrograming/FunctionalProgramingAPI/Covid/views.py
+++ CovidAnalysis/FunctionalPrograming/FunctionalProgramingAPI/Covid/views.py
@@ -1,3 +1,5 @@
+import time
+from django.core import serializers
 from django.shortcuts import render
 from rest_framework.response import Response
 from rest_framework.decorators import action
@@ -67,12 +69,6 @@ class Covid(ViewSet):
         first_countries, first_max = annotate_countries(first_period)
         second_countries, second_max = annotate_countries(second_period)
 
-        # result = [
-        #     first.name for first, second
-        #     in zip(second_countries, first_countries)
-        #     if categorize(first.avg_new_cases, first_max) > categorize(second.avg_new_cases, second_max)
-        # ]
-        print('don?')
         result = [
             {
                 'name': first.name,
@@ -85,3 +81,44 @@ class Covid(ViewSet):
 
         ]
         return Response(data=result, status=status.HTTP_200_OK)
+
+    @action(methods=['get'], detail=False)
+    def get_entries(self, request, *args, **kwargs):
+        try:
+            continents = Continent.objects.all()
+
+            if 'continent_name' in request.data.keys():
+                continent_name = request.data['continent_name']
+                continents = continents.filter(name=continent_name)
+
+            if 'country_name' in request.data.keys():
+                country_name = request.data['country_name']
+                continents = continents.filter(countries__name=country_name)
+
+            if 'period' in request.data.keys():
+                period = request.data['period']
+                continents = continents.filter(
+                    countries__entries__date__range=period)
+
+            start = time.time()
+            result = [{
+                'continent': continent.name,
+                'countries': [{
+                    'country': country.name,
+                    'entries': [
+                        {
+                            'date': entry.date,
+                            'new_cases': entry.new_cases,
+                            'new_cases_per_million': entry.new_cases_per_million,
+                            'new_deaths': entry.new_deaths,
+                        } for entry in country.entries.all()]
+                }for country in continent.countries.all()]
+            } for continent in continents]
+
+            print(f'time: {time.time()-start}')
+
+            return Response(data=result, status=status.HTTP_200_OK)
+
+        except Exception as e:
+            print(e)
+            return Response(status=status.HTTP_500_INTERNAL_SERVER_ERROR)
