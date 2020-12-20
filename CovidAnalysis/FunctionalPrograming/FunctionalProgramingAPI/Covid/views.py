fd43a4736f5eb6f5c5d8f886d17bf14e7cd6ac79
Raff-dev <56380303+Raff-dev@users.noreply.github.com>
2020-12-20 01:44:27 +0100

    update get entries returns nested structure | enable to filter by multiple continents/countries

--- CovidAnalysis/FunctionalPrograming/FunctionalProgramingAPI/Covid/views.py
+++ CovidAnalysis/FunctionalPrograming/FunctionalProgramingAPI/Covid/views.py
@@ -44,17 +44,16 @@ class Covid(ViewSet):
 
     @action(methods=['get'], detail=False)
     def get_ranked_up(self, request, *args, **kwargs):
-        continent_name = request.data['continent_name']
+        continent_names = request.data['continent_names']
         first_period = request.data['first_period']
         second_period = request.data['second_period']
-        print('don1?')
 
         continent_countries = Country.objects.filter(
             Exists(Entry.objects.filter(
                 country__id=OuterRef('id'), date__range=first_period)),
             Exists(Entry.objects.filter(
                 country__id=OuterRef('id'), date__range=second_period)),
-            continent__name=continent_name)
+            continent__name__in=continent_names)
 
         def categorize(avg, max_avg):
             return 1 if avg < max_avg/3 else (2 if avg < max_avg/3*2 else 3)
@@ -71,54 +70,51 @@ class Covid(ViewSet):
 
         result = [
             {
-                'name': first.name,
-                'category': [categorize(first.avg_new_cases, first_max), categorize(second.avg_new_cases, second_max)],
-                'avg':[first.avg_new_cases, second.avg_new_cases],
+                'first_max_avg_cases': first_max,
+                'second_max_avg_cases': second_max,
+                'countries': [{
+                    'name': first.name,
+                    'category': [categorize(first.avg_new_cases, first_max), categorize(second.avg_new_cases, second_max)],
+                    'avg':[first.avg_new_cases, second.avg_new_cases],
 
-            } for first, second
-            in zip(first_countries, second_countries)
-            if categorize(first.avg_new_cases, first_max) > categorize(second.avg_new_cases, second_max)
-
+                } for first, second
+                    in zip(first_countries, second_countries)
+                    if categorize(first.avg_new_cases, first_max) > categorize(second.avg_new_cases, second_max)]
+            }
         ]
+
         return Response(data=result, status=status.HTTP_200_OK)
 
     @action(methods=['get'], detail=False)
     def get_entries(self, request, *args, **kwargs):
-        try:
-            continents = Continent.objects.all()
+        continents = Continent.objects.all()
 
-            if 'continent_name' in request.data.keys():
-                continent_name = request.data['continent_name']
-                continents = continents.filter(name=continent_name)
+        if 'continent_names' in request.data.keys():
+            continent_names = request.data['continent_names']
+            continents = continents.filter(name__in=continent_names)
 
-            if 'country_name' in request.data.keys():
-                country_name = request.data['country_name']
-                continents = continents.filter(countries__name=country_name)
+        if 'country_names' in request.data.keys():
+            country_names = request.data['country_names']
+            continents = continents.filter(countries__name__in=country_names)
 
-            if 'period' in request.data.keys():
-                period = request.data['period']
-                continents = continents.filter(
-                    countries__entries__date__range=period)
+        if 'period' in request.data.keys():
+            period = request.data['period']
+            continents = continents.filter(
+                countries__entries__date__range=period)
 
-            start = time.time()
-            result = [{
-                'continent': continent.name,
-                'countries': [{
-                    'country': country.name,
-                    'entries': [
-                        {
-                            'date': entry.date,
-                            'new_cases': entry.new_cases,
-                            'new_cases_per_million': entry.new_cases_per_million,
-                            'new_deaths': entry.new_deaths,
-                        } for entry in country.entries.all()]
-                }for country in continent.countries.all()]
-            } for continent in continents]
+        start = time.time()
+        result = [{
+            'continent': continent.name,
+            'countries': [{
+                'country': country.name,
+                'entries': [
+                    {
+                        'date': entry.date,
+                        'new_cases': entry.new_cases,
+                        'new_cases_per_million': entry.new_cases_per_million,
+                        'new_deaths': entry.new_deaths,
+                    } for entry in country.entries.all()]
+            } for country in continent.countries.all()]
+        } for continent in continents]
 
-            print(f'time: {time.time()-start}')
-
-            return Response(data=result, status=status.HTTP_200_OK)
-
-        except Exception as e:
-            print(e)
-            return Response(status=status.HTTP_500_INTERNAL_SERVER_ERROR)
+        return Response(data=result, status=status.HTTP_200_OK)
